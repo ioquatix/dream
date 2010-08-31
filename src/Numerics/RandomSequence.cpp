142a7c37035487f79fb049ed685f5c857d83b1ea
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2010-09-01 01:05:46 +1200

    Added RandomSequence class to Numerics.

---
+++ src/Numerics/RandomSequence.cpp
@@ -0,0 +1,25 @@
+/*
+ *  RandomPointSequence.cpp
+ *  Dream
+ *
+ *  Created by Administrator on 17/02/08.
+ *  Copyright 2008 Samuel Williams. All rights reserved.
+ *
+ */
+
+#include "RandomSequence.h"
+
+namespace Dream {
+	namespace Numerics {
+
+		RandomSequence::RandomSequence (unsigned seed, RealT min, RealT max)
+		: m_sequence(BaseGeneratorT(seed), DistributionT (min, max)) {
+		
+		}
+
+		const RealT RandomSequence::getReal () {
+			return m_sequence();
+		}
+		
+	}
+}
---
+++ src/Numerics/RandomSequence.h
@@ -0,0 +1,44 @@
+/*
+ *  RandomPointSequence.h
+ *  Dream
+ *
+ *  Created by Administrator on 17/02/08.
+ *  Copyright 2008 Samuel Williams. All rights reserved.
+ *
+ */
+ 
+#include "Vector.h"
+#include <boost/random.hpp>
+
+namespace Dream {
+	namespace Numerics {
+		
+		class RandomSequence {
+		protected:
+			typedef boost::uniform_real<> DistributionT;		
+			typedef boost::minstd_rand BaseGeneratorT;
+			typedef boost::variate_generator<BaseGeneratorT, DistributionT> GeneratorT;
+			
+			GeneratorT m_sequence;
+		
+			Vec3 m_currentPoint;
+			Vec3 m_scale;
+		public:
+			RandomSequence (unsigned seed, RealT min = -1.0, RealT max = 1.0);
+			
+			template <unsigned D>
+			const Vector<D> getVector () {
+				Vector<D> result;
+				
+				for (std::size_t i = 0; i < D; i++) {
+					result[i] = getReal();
+				}
+				
+				return result;
+			}
+			
+			const RealT getReal ();			
+		};
+		
+	}
+}
