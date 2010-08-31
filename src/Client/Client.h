78ea0f37c320bb99e2739bba117f256826822e60
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2010-09-01 01:07:34 +1200

    Added Client.h for documentation purposes.

---
+++ src/Client/Client.h
@@ -0,0 +1,24 @@
+/*
+ *  Client.h
+ *  Dream
+ *
+ *  Created by Samuel Williams on 14/08/10.
+ *  Copyright 2010 Orion Transfer Ltd. All rights reserved.
+ *
+ */
+
+#ifndef _DREAM_CLIENT_CLIENT_H
+#define _DREAM_CLIENT_CLIENT_H
+
+namespace Dream
+{
+	/** The Client namespace is functionality which is specific to actual graphical applications.
+	
+	This includes such functionality such as graphics, audio, user interface, and associated areas.	
+	*/
+	namespace Client
+	{
+	}
+}
+
+#endif
