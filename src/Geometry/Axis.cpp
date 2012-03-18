52f6ed7be2afaa6f9475d88201b7b9264b42d236
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-03-18 18:35:32 +1300

    Axis class for structured geometry transformations.

---
+++ src/Geometry/Axis.cpp
@@ -0,0 +1,15 @@
+//
+//  Axis.cpp
+//  Dream
+//
+//  Created by Samuel Williams on 12/03/12.
+//  Copyright (c) 2012 Orion Transfer Ltd. All rights reserved.
+//
+
+#include "Axis.h"
+
+namespace Dream {
+	namespace Geometry {
+		
+	}	
+}
