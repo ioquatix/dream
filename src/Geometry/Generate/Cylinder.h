5ab73e9625541a92c7c8758645fd1f31bdba53ed
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-06-15 06:28:32 +1200

    Basic cylinder generation code is working again.

--- src/Geometry/Generate/Cylinder.h
+++ src/Geometry/Generate/Cylinder.h
@@ -17,13 +17,13 @@ namespace Dream {
 	namespace Geometry {
 		namespace Generate {
 			
-			RealT calculate_rotation_length (const unsigned rotation, const std::vector<Vec3> &left, const std::vector<Vec3 > &right) {
+			RealT calculate_rotation_length (const std::size_t rotation, const std::vector<Vec3> &left, const std::vector<Vec3> &right) {
 				RealT length = 0;
 				
 				Vec3 centers[2];
 				centers[0].zero(); centers[1].zero();
 				
-				for (unsigned i = 0; i < left.size(); i += 1) {
+				for (std::size_t i = 0; i < left.size(); i += 1) {
 					centers[0] += left[i];
 					centers[1] += right[i];
 				}
@@ -33,7 +33,7 @@ namespace Dream {
 				
 				Vec3 segment_direction = (centers[1] - centers[0]).normalize();
 				
-				for (unsigned i = 0; i < left.size(); i += 1) {
+				for (std::size_t i = 0; i < left.size(); i += 1) {
 					RealT cosl = segment_direction.dot((right[(i + rotation) % right.size()] - left[i]).normalize());
 					length += 1.0 - cosl;
 				}
@@ -42,16 +42,16 @@ namespace Dream {
 			}
 			
 			int calculate_rotation_offset (const std::vector<Vec3> &left, const std::vector<Vec3> &right) {
-				assert(left.size() == right.size());
+				DREAM_ASSERT(left.size() == right.size());
 				std::vector<RealT> lengths(left.size());
 				
-				for (unsigned i = 0; i < right.size(); i++) {
+				for (std::size_t i = 0; i < right.size(); i++) {
 					lengths.at(i) = calculate_rotation_length(i, left, right);
 				}
 				
 				int index = 0;
 				RealT maximum = lengths[0];
-				for (unsigned i = 1; i < lengths.size(); ++i) {
+				for (std::size_t i = 1; i < lengths.size(); ++i) {
 					if (maximum > lengths[i]) {
 						index = i;
 						maximum = lengths[i];
@@ -61,58 +61,139 @@ namespace Dream {
 				return index;
 			}
 			
+			/// Build a tube out of a set of stacks of a given number of slices:
 			template <typename MeshT>
-			void cylinder(const MeshT & mesh, const ISpline<3> * spline, RealT radius, unsigned slices, bool cap_start, bool cap_end) {
-				std::vector<RealT> times = spline->times_at_resolution(20, 2);
-				std::vector<std::vector<std::size_t>> indices(slices);
+			void tube(MeshT & mesh, std::size_t slices) {
+				for (std::size_t i = 0; i < (mesh.vertices.size() - slices); i += slices) {
+					for (std::size_t j = 0; j < slices; j += 1) {
+						std::size_t k = (j + 1) % slices;
+						
+						std::size_t i1 = i + j, j1 = (i + slices) + j;
+						std::size_t i2 = i + k, j2 = (i + slices) + k;
+						
+						mesh << i1 << i2 << j2;
+						mesh << i1 << j2 << j1;
+					}
+				}
+			}
+			
+			template <typename MeshT>
+			void cylinder(MeshT & mesh, const RealT &base_radius, const RealT &top_radius, const RealT &height, const std::size_t &slices, const std::size_t &stacks, bool cap_start, bool cap_end) {
 				
-				for (unsigned c = 0; c < slices; ++c) {
-					RealT rotation = ROTATION_360 * ((RealT)c / (RealT)slices);
+				mesh.layout = TRIANGLES;
+				
+				Vec3 base (0, base_radius, 0);
+				Vec3 top (height, top_radius, 0);
+				Vec3 center(1, 0, 0);
+				
+				DREAM_ASSERT((base_radius != 0.0 || top_radius != 0.0) && "Both base_radius and top_radius are zero!");
+				
+				auto rotation = Mat44::rotating_matrix(-R360 / slices, center);
+				
+				// N stacks has n+1 divisions, one for the start, one for the end
+				std::size_t divisions = stacks + 1;
+				
+				//auto index = array_index<2>(vec(divisions, slices));
+				//ArrayIndex<2> index(vec(divisions, slices));
+				//index(mesh->vertices)[y][x]
+				
+				mesh.vertices.reserve(divisions * slices);
+				
+				// Generate all the points for the stacks and slices of the cylinder
+				for (std::size_t stk = 0; stk < divisions; ++stk) {
+					Vec3 current = linear_interpolate((RealT)stk / (RealT)stacks, base, top);		
 					
-					for (auto time : times) {
-						Vec3 v = spline->point_at_time(time);
-						Vec3 tangent (spline->tangent_at_time(time));
+					for (std::size_t slice = 0; slice < slices; ++slice) {
+						typename MeshT::VertexT vertex;
+						vertex.position = current;
 						
-						Vec3 pt = Matrix4x4::rotatingMatrix(-rotation, tangent) * spline->normal_at_time(time) * radius;
+						mesh << vertex;
 						
-						std::size_t index = mesh->addVertex(v + pt);
-						indices[c].push_back(index);
+						current = rotation * current;
 					}
 				}
 				
-				int rotation = 0;
-				///iterate over each stack idx
-				for (unsigned i = 0; i < (indices[0].size() - 1); i += 1) {
-					std::vector<index_t> left, right;
-					//indices[rotation around][stack idx]
+				/*
+				// If we are capping, add two more points for the center points at each end
+				if (cap_start || cap_end) {
+					typename MeshT::VertexT vertex;
+					vertex.position.zero();
 					
-					for (unsigned j = 0; j < indices.size(); j++) {
-						left.push_back(indices.at(j).at(i));
-						right.push_back(indices.at(j).at((i+1) % indices[0].size()));
+					mesh.vertices.push_back(vertex);
+					
+					vertex.position[X] = height;
+					mesh.vertices.push_back(vertex);
+					
+					unsigned base = 0, top = (divisions - 1) * slices;
+					unsigned centers = mesh.vertices.size() - 2;
+					
+					for (std::size_t slice = 0; slice < slices; ++slice) {
+						// Add in triangles for the caps
+						if (cap_start && base_radius > 0.0) {
+							mesh.indices.push_back(centers+0);
+							mesh.indices.push_back(base + ((slice+1) % slices));
+							mesh.indices.push_back(base + slice);
+						}
+						
+						if (cap_end && top_radius > 0.0) {
+							mesh.indices.push_back(centers+1);
+							mesh.indices.push_back(top + slice);
+							mesh.indices.push_back(top + ((slice+1) % slices));
+						}
 					}
+				}
+				*/
+				
+				/*
+				// Add in the quad surfaces for the cylinder
+				for (std::size_t stk = 0; stk < stacks; ++stk) {
+					std::size_t stack_bottom_index = stk * slices;
+					std::size_t stack_top_index = (stk + 1) * slices;
 					
-					rotation = calculateRotationOffset(mesh->vertices(left), mesh->vertices(right));
-					
-					for (unsigned c = 0; c < indices.size(); c += 1) {
-						unsigned c2 = (c+1) % indices.size(); //rotation bottom
-						unsigned i2 = (i+1) % indices[0].size();
+					for (std::size_t slice = 0; slice < slices; slice += 1) {
+						std::size_t slice_current_offset = slice;
+						std::size_t slice_next_offset = (slice + 1) % slices;
 						
-						index_t idxs[2][2];
-						idxs[0][0] = indices.at((c2) % indices.size()).at(i);
-						idxs[0][1] = indices.at((c) % indices.size()).at(i);
-						idxs[1][0] = indices.at((c2+rotation) % indices.size()).at(i2); 
-						idxs[1][1] = indices.at((c+rotation) % indices.size()).at(i2);
+						if (base_radius == 0.0 && stk == 0) {
+							mesh.indices.push_back(stack_top_index + slice_current_offset);
+							mesh.indices.push_back(stack_bottom_index + slice_current_offset);
+							mesh.indices.push_back(stack_top_index + slice_next_offset);
+						} else if (top_radius == 0.0 && stk == (stacks-1)) {
+							mesh.indices.push_back(stack_top_index + slice_current_offset);
+							mesh.indices.push_back(stack_bottom_index + slice_current_offset);
+							mesh.indices.push_back(stack_bottom_index + slice_next_offset);
+						} else {
+							mesh.indices.push_back(stack_top_index + slice_current_offset);
+							mesh.indices.push_back(stack_bottom_index + slice_current_offset);
+							mesh.indices.push_back(stack_bottom_index + slice_next_offset);
+							
+							mesh.indices.push_back(stack_top_index + slice_current_offset);
+							mesh.indices.push_back(stack_bottom_index + slice_next_offset);
+							mesh.indices.push_back(stack_top_index + slice_next_offset);
+						}
+					}
+				}
+				*/
+				
+				tube(mesh, slices);
+			}
+			
+			/// Construct a tube out of a series of splines:
+			template <typename MeshT, typename SplineT>
+			void cylinder(MeshT & mesh, const std::vector<SplineT> & splines, RealT resolution) {
+				std::size_t slices = splines.size();
+				resolution = 1.0 / resolution;
+				
+				for (RealT i = 0.0; i <= 1.0; i += resolution) {
+					for (std::size_t c = 0; c < slices; c += 1) {
+						typename MeshT::VertexT vertex;
+						vertex.position = splines[c].point_at_time(i);
 						
-						mesh->addTriangleSurface(idxs[0][0], idxs[1][0], idxs[0][1]);
-						mesh->addTriangleSurface(idxs[0][1], idxs[1][0], idxs[1][1]);
-						
-						mesh->vertex(idxs[0][1])->setColor(vec(0.0, 1.0, 0.0, 1.0));
+						mesh << vertex;
 					}
 				}
 				
-				for (unsigned k = 0; k < indices[0].size(); k++) {
-					mesh->vertex(indices[0][k])->setColor(vec(1.0, 0.0, 0.0, 1.0));
-				}
+				tube(mesh, splines.size());
 			}
 			
 		}
