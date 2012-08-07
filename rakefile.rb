7089cd5ef8fdc3fa1d1b145f41a97271bee2eb09
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-08-07 18:40:32 +1200

    Added support for uncrustify.

--- rakefile.rb
+++ rakefile.rb
@@ -3,6 +3,7 @@
 require 'stringio'
 require 'fileutils'
 require 'yaml'
+require 'pathname'
 
 PROJECT_PATH = File.dirname(__FILE__)
 SOURCE_PATH = File.join(PROJECT_PATH, "src")
@@ -11,4 +12,9 @@ CPP_SOURCE = ['h', 'c', 'cpp']
 
 Dir["./tasks/*.rake"].each do |path|
 	load path
+end
+
+def run(*args)
+	$stderr.puts args.join(' ')
+	system(*args.collect{|arg| arg.to_s})
 end
