5c746739c2ca686411bad5b3ac5884e368df5f2f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-08-23 23:05:18 +1200

    Moved some Cylinder generation related code now using non-header implementation.

--- src/Geometry/Generate/Cylinder.h
+++ src/Geometry/Generate/Cylinder.h
@@ -16,50 +16,9 @@
 namespace Dream {
 	namespace Geometry {
 		namespace Generate {
-			RealT calculate_rotation_length (const std::size_t rotation, const std::vector<Vec3> &left, const std::vector<Vec3> &right) {
-				RealT length = 0;
-
-				Vec3 centers[2];
-				centers[0].zero(); centers[1].zero();
-
-				for (std::size_t i = 0; i < left.size(); i += 1) {
-					centers[0] += left[i];
-					centers[1] += right[i];
-				}
-
-				centers[0] /= left.size();
-				centers[1] /= right.size();
-
-				Vec3 segment_direction = (centers[1] - centers[0]).normalize();
-
-				for (std::size_t i = 0; i < left.size(); i += 1) {
-					RealT cosl = segment_direction.dot((right[(i + rotation) % right.size()] - left[i]).normalize());
-					length += 1.0 - cosl;
-				}
-
-				return length;
-			}
-
-			int calculate_rotation_offset (const std::vector<Vec3> &left, const std::vector<Vec3> &right) {
-				DREAM_ASSERT(left.size() == right.size());
-				std::vector<RealT> lengths(left.size());
-
-				for (std::size_t i = 0; i < right.size(); i++) {
-					lengths.at(i) = calculate_rotation_length(i, left, right);
-				}
-
-				int index = 0;
-				RealT maximum = lengths[0];
-				for (std::size_t i = 1; i < lengths.size(); ++i) {
-					if (maximum > lengths[i]) {
-						index = i;
-						maximum = lengths[i];
-					}
-				}
-
-				return index;
-			}
-
+			RealT calculate_rotation_length (const std::size_t rotation, const std::vector<Vec3> &left, const std::vector<Vec3> &right);
+			std::size_t calculate_rotation_offset (const std::vector<Vec3> &left, const std::vector<Vec3> &right);
+			
 			/// Build a tube out of a set of stacks of a given number of slices:
 			template <typename MeshT>
 			void tube(MeshT & mesh, std::size_t slices) {
