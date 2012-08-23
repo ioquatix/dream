349a0c98a97c1395eec6fc2c7fb777004c40cbc5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-08-23 23:05:51 +1200

    Improved OpenCV build script.

--- ext/packages/OpenCV-2.4.2/build.rb
+++ ext/packages/OpenCV-2.4.2/build.rb
@@ -7,7 +7,7 @@ Package.define("OpenCV-2.4.2") do |package|
 				sh("mkdir", "build")
 				
 				Dir.chdir("build") do
-					sh("cmake", "-G", "Unix Makefiles", "-DCMAKE_INSTALL_PREFIX:PATH=#{platform.prefix}", "-DCMAKE_CXX_COMPILER_WORKS=TRUE", "-DCMAKE_C_COMPILER_WORKS=TRUE", "-DBUILD_opencv_legacy=OFF", "-DBUILD_opencv_video=OFF", "-DBUILD_opencv_ts=OFF", "-DBUILD_opencv_ts=OFF", "-DBUILD_opencv_highgui=OFF", "-DBUILD_SHARED_LIBS=OFF", "..")
+					sh("cmake", "-G", "Unix Makefiles", "-DCMAKE_INSTALL_PREFIX:PATH=#{platform.prefix}", "-DCMAKE_CXX_COMPILER_WORKS=TRUE", "-DCMAKE_C_COMPILER_WORKS=TRUE", "-DBUILD_opencv_legacy=OFF", "-DBUILD_opencv_nonfree=OFF", "-DBUILD_opencv_ts=OFF", "-DBUILD_opencv_ts=OFF", "-DBUILD_opencv_highgui=OFF", "-DBUILD_SHARED_LIBS=OFF", "..")
 					
 					sh("make")
 					sh("make", "install")
